462cd5be76c1b788eed788fcdeb832e4c4011170
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-31 22:49:03 +0900

    Feat: Add base files and packages

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<MemoApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MemoApp from './components/MemoApp.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		MemoApp,
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/MemoApp.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>MemoApp</div>
+</template>
+
+<script>
+export default {
+	name: 'MemoApp',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
