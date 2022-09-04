4ee7037f120e9611dc8fd636a1628e2f8ee6c449
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-09-04 12:24:21 +0900

    Feat: Show memo content

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container">メモリスト</div>
-  <div>メモ1</div>
-  <div>メモ2</div>
+  <ul>
+    <li v-for="memo in memos" :key="memo.id">
+      {{ memo.content }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "MemoList",
+  computed: {
+    memos() {
+      return this.$store.state.memos;
+    },
+  },
 };
 </script>
 
