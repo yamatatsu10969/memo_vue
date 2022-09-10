75a95c908eecedc64d9b770a933cabdab914233a
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-09-10 17:37:22 +1000

    Feat: Add button css

--- src/components/MemoForm.vue
+++ src/components/MemoForm.vue
@@ -1,13 +1,30 @@
 <template>
   <div><textarea v-model="content" name="memo-form" id="memo-form" cols="30" rows="10"></textarea></div>
-  <div><button @click="save">保存</button></div>
+  <div><button class="btn btn-primary" @click="save">保存</button></div>
 </template>
 
 <script>
 export default {
   name: "MemoForm",
   props: ["memo"],
+  forceUpdate() {
+    console.log("forceUpdate");
+    return;
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  updated() {
+    console.log("updated");
+  },
+  /// render した後にここは一度しか通らない
+  /// また render をかける方法がわからない
+  /// edit/** を何回か行くとデータが変わらない
   data() {
+    console.log(this.memo.content);
     return {
       content: this.memo.content,
     };
